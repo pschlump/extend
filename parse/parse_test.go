9ef564ad57adf9143dcb6f0f8bf89a07ee0fd57c
Philip Schlump <pschlump@gmail.com>
2020-05-14 09:47:02 -0600

    Copyright notice

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Copyright 2020 Philip Schlump. All rights reserved.
+// Same license as the Go Authors in the LICENSE file.
+
 package parse
 
 import (
