9ec7e3491e3150a01832fa63856241915690f5bd
Philip Schlump <pschlump@gmail.com>
2020-05-19 19:04:13 -0600

    Add library of utility functions

---
+++ sprig/network.go
@@ -0,0 +1,12 @@
+package extendsprig
+
+import (
+	"math/rand"
+	"net"
+)
+
+func getHostByName(name string) string {
+	addrs, _ := net.LookupHost(name)
+	//TODO: add error handing when release v3 cames out
+	return addrs[rand.Intn(len(addrs))]
+}
